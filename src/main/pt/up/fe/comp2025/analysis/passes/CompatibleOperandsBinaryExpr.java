e135dfa3cf173648cd03be728e7958d45dd08e79
cosmintianu <cosmin.tianunicolae@yahoo.com>
2025-03-10 17:34:28 +0000

    Added semantic pass so the 2 operands of a binary expr are compatible

---
+++ src/main/pt/up/fe/comp2025/analysis/passes/CompatibleOperandsBinaryExpr.java
@@ -0,0 +1,47 @@
+package pt.up.fe.comp2025.analysis.passes;
+
+import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
+import pt.up.fe.comp.jmm.ast.JmmNode;
+import pt.up.fe.comp.jmm.report.Report;
+import pt.up.fe.comp.jmm.report.Stage;
+import pt.up.fe.comp2025.analysis.AnalysisVisitor;
+import pt.up.fe.comp2025.ast.Kind;
+
+public class CompatibleOperandsBinaryExpr extends AnalysisVisitor {
+    @Override
+    protected void buildVisitor() {
+        addVisit(Kind.BINARY_EXPR, this::visitBinaryExpr);
+    }
+
+    private Void visitBinaryExpr(JmmNode binaryExpr, SymbolTable table) {
+        // Get the operands
+        JmmNode leftOperand = binaryExpr.getChild(0);
+        JmmNode rightOperand = binaryExpr.getChild(1);
+
+        String leftType = leftOperand.getKind();
+        String rightType = rightOperand.getKind();
+
+        System.out.println("Left operand type: " + leftType);
+        System.out.println("Right operand type: " + rightType);
+        System.out.println("Number of children: " + binaryExpr.getChildren().size());
+
+        System.out.println(binaryExpr);
+
+        if (leftType.equals(Kind.INTEGER_LITERAL.toString()) &&
+                leftType.equals(rightType)) {
+            return null;
+        }
+
+        // Create error report
+        var message = String.format("Binary expression has type %s and left type %s", leftType, rightType);
+        addReport(Report.newError(
+                Stage.SEMANTIC,
+                binaryExpr.getLine(),
+                binaryExpr.getColumn(),
+                message,
+                null)
+        );
+
+        return null;
+    }
+}
